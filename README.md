0426f819450ede8617f62002ce418f27cc2d2d93
Sam Goodwin <sam.goodwin1989@gmail.com>
2019-12-30 03:08:52 -0800

    Initial commit supporting v0.11.0 of Punchcard

---
+++ README.md
@@ -0,0 +1,5 @@
+# punchcard-example
+
+This is a sample project for deploying AWS applications with [Punchcard](https://github.com/punchcard/punchcard).
+
+It invokes a "Hello, World" Lambda Function once per minute with a scheduled CloudWatch Event.
---
+++ index.ts
@@ -0,0 +1,15 @@
+
+import cdk = require('@aws-cdk/core');
+import { Schedule } from '@aws-cdk/aws-events';
+import { Core, Lambda } from 'punchcard';
+
+export const app = new Core.App();
+const stack = app.root.map(app => new cdk.Stack(app, 'hello-world'));
+
+Lambda.schedule(stack, 'MyFunction', {
+  schedule: Schedule.rate(cdk.Duration.minutes(1))
+}, async() => {
+  // say hello once per minute with a scheduled Lambda Function invocation
+  console.log('Hello, World!');
+});
+
